8d56d62f3a2e23cecde5901996b37a1039ccdab1
Roberto Luiz Veiga Junior <robertoluizveigajunior@Mac-mini-de-Roberto.local>
2016-06-30 00:15:41 -0300

    first commit

---
+++ Pods/Target Support Files/Pods-Progresso/Pods-Progresso-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Progresso : NSObject
+@end
+@implementation PodsDummy_Pods_Progresso
+@end
---
+++ Pods/Target Support Files/Pods-Progresso/Pods-Progresso-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_ProgressoVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_ProgressoVersionString[];
+
