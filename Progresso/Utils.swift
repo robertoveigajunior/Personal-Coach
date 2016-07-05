0fc7072f925f70476c5c180ecb05e466dfdfae59
Roberto Luiz Veiga Junior <robertoluizveigajunior@Mac-mini-de-Roberto.local>
2016-07-04 23:53:55 -0300

    Alamofire deployment

---
+++ Progresso/Utils.swift
@@ -0,0 +1,13 @@
+//
+//  Utils.swift
+//  Progresso
+//
+//  Created by Roberto Luiz Veiga Junior on 04/07/16.
+//  Copyright © 2016 robertoluizveigajunior. All rights reserved.
+//
+
+import UIKit
+
+class Utils: NSObject {
+
+}
